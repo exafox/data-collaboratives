b495551f9ebda6abbe4dc71aeb72a5e498946bdf
Ena Bek <enabek@gmail.com>
2017-01-10 16:43:28 -0500

    Update 2017-01-10T21:43:27.174Z

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
+  var currentScroll = 0;
+
+  function lockscroll(){
+    $(window).scrollTop(currentScroll);
+  }
   // Modal Click Behavior
   $('.js-open-modal').click(function () {
       $(this).find('.js-target-modal').addClass('js-active');
+      currentScroll=$(window).scrollTop();
+      $(window).bind('scroll',lockscroll);
       $('#overlay').addClass('js-active');
       $('body').addClass('js-body-modal-active');
   });
 
-  $('body').on ("click", '.js-close-modal', function () {
+  // Prevent default on footer close
+  $('footer .js-close-modal').on('click', function(e) {
+    e.preventDefault();
+  })
+
+  $('body').on("click", '.js-close-modal', function () {
+      currentScroll=$(window).scrollTop();
+      $(window).unbind('scroll');
       $('.js-target-modal').removeClass('js-active');
       $('#overlay').removeClass('js-active');
       $('body').removeClass('js-body-modal-active');
@@ -15,6 +29,8 @@ $(document).ready(function(){
   // General Click Behavior for Overlay
   $('#overlay').click(function () {
       $('.js-active').removeClass('js-active');
+      currentScroll=$(window).scrollTop();
+      $(window).unbind('scroll');
       $('.js-active-menu').removeClass('js-active-menu');
       $('body').removeClass('js-body-modal-active');
   });
