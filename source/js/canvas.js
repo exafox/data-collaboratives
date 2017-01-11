720a061f70a73002da1424e2712e512d8cbc5347
Denny Tek <tek.denny@gmail.com>
2017-01-11 13:21:21 -0500

    Add routing by url hash to canvas

--- source/js/canvas.js
+++ source/js/canvas.js
@@ -4,16 +4,27 @@ $(document).ready(function() {
 
   // title-slider handler
   $('body').on('click', '.title-slider-nav', function() {
-    var phaseTarget = $(this).attr("data-target");
-    $('.phase-wrapper').hide();
-    $(phaseTarget).show();
+    var phaseTarget = $(this).attr("data-target") || "#phase-1";
+    displayPhases(phaseTarget);
   });
 
   // Canvas arrows
   $('.title-slider').on("afterChange", function() {
-    var phaseTarget = $(this).find('.slick-current').attr('data-target');
-    $('.phase-wrapper').hide();
-    $(phaseTarget).show();
+    var phaseTarget = $(this).find('.slick-current').attr('data-target') || "#phase-1";
+    displayPhases(phaseTarget);
   });
+
+  // url map to phase
+  if (window.location.hash !== '') {
+    var urlPhaseTarget = window.location.hash;
+    displayPhases(urlPhaseTarget);
+  }
+
+  function displayPhases(target) {
+    $('.phase-wrapper').hide();
+    $(target).show();
+    window.location.hash = target;
+    $(document).scrollTop(0);
+  }
 
 });
