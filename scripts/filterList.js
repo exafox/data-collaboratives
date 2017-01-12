d996e2ed5bd6a055eb40ee035bada939650cf054
Ena Bek <enabek@gmail.com>
2017-01-12 17:04:54 -0500

    Update 2017-01-12T22:04:53.249Z

--- scripts/filterList.js
+++ scripts/filterList.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
 
   $(".api").on('click', function() {
-    window.location.href = '/cases.html?#api';
+    window.location.href = '/explorer.html?#api';
   });
 
   $(".trusted-intermediary").on('click', function() {
-    window.location.href = '/cases.html?#trusted-intermediary';
+    window.location.href = '/explorer.html?#trusted-intermediary';
   });
 
   $(".data-pooling").on('click', function() {
-    window.location.href = '/cases.html?#data-pooling';
+    window.location.href = '/explorer.html?#data-pooling';
   });
 
   $(".prizes-challenges").on('click', function() {
-    window.location.href = '/cases.html?#prizes-challenges';
+    window.location.href = '/explorer.html?#prizes-challenges';
   });
 
   $(".research-partnerships").on('click', function() {
-    window.location.href = '/cases.html?#research-partnerships';
+    window.location.href = '/explorer.html?#research-partnerships';
   });
 
   $(".intelligence-product").on('click', function() {
-    window.location.href = '/cases.html?#intelligence-product';
+    window.location.href = '/explorer.html?#intelligence-product';
   });
 
   $("h2#data-filter-container span").on('click', function() {
     var elm = $(this).children();
     var elmClass = elm.attr("class");
-    window.location.href = '/cases.html?#' + elmClass;
+    window.location.href = '/explorer.html?#' + elmClass;
   });
 
 })
