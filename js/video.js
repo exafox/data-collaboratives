b81eeef9710ab8c22fb0dc60d44960f19147e9f7
mocxd <mark.adkins.hastings@gmail.com>
2017-01-17 05:53:16 -0500

    Update 2017-01-17T10:52:47.508Z

--- js/video.js
+++ js/video.js
@@ -1,20 +1,20 @@
 $(window).load(function() {
   var screenWidth = $(window).width();
 
-  if (screenWidth <= 1280) {
+  if (screenWidth <= 1200) {
     $(".top-section--bg").css("background-image", "url('/images/video/long-img.png')");
-  } else if (screenWidth > 1280) {
+  } else {
     $(".top-section--bg").css("background-image", "none");
     $(".top-section--bg").css("background-color", "transparent");
   }
-})
+});
 
 $(window).resize(function() {
   var screenWidth = $(window).width();
 
-  if (screenWidth <= 1280) {
+  if (screenWidth <= 1200) {
     $(".top-section--bg").css("background-image", "url('/images/video/long-img.png')");
-  } else if (screenWidth > 1280) {
+  } else {
     $(".top-section--bg").css("background-image", "none");
     $(".top-section--bg").css("background-color", "transparent");
   }
